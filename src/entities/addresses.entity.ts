efd0ba6ef6d8c0dc32677325d811b1b8cfd58d93
leticiamontilha <leticiammontilha@hotmail.com>
2023-03-01 19:33:52 -0300

    create: entity addresses

---
+++ src/entities/addresses.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Entity, 
+    Column, 
+    PrimaryGeneratedColumn
+ } from "typeorm"
+
+
+ @Entity('addresses')
+ 
+export class address {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number
+
+    @Column({length: 45})
+    street: string 
+
+    @Column({length: 8})
+    zipCode: string
+
+    @Column({ length: 6, nullable: true})
+    number: string
+
+    @Column({length: 20})
+    coty: string
+
+    @Column({length: 2})
+    state: string
+ }
+
+ 
